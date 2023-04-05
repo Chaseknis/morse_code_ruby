091582717e1f2912a44e3a63b7423f162e1a8d93
chaseknis <chase6th@gmail.com>
2023-04-05 16:16:08 +0200

    Update README.md file Update index.rb file Check for linters errors

--- index.rb
+++ index.rb
@@ -1,51 +1,65 @@
-morse_to_alpha = {
-    ".-" => "a",
-    "-..." => "b",
-    "-.-." => "c",
-    "-.." => "d",
-    "." => "e",
-    "..-." => "f",
-    "--." => "g",
-    "...." => "h",
-    ".." => "i",
-    ".---" => "j",
-    "-.-" => "k",
-    ".-.." => "l",
-    "--" => "m",
-    "-." => "n",
-    "---" => "o",
-    ".--." => "p",
-    "--.-" => "q",
-    ".-." => "r",
-    "..." => "s",
-    "-" => "t",
-    "..-" => "u",
-    "...-" => "v",
-    ".--" => "w",
-    "-..-" => "x",
-    "-.--" => "y",
-    "--.." => "z",
-    ".----" => "1",
-    "..---" => "2",
-    "...--" => "3",
-    "....-" => "4",
-    "....." => "5",
-    "-...." => "6",
-    "--..." => "7",
-    "---.." => "8",
-    "----." => "9",
-    "-----" => "0"
-  }
-
+MORSE_CODE = {
+  '.-' => 'a',
+  '-...' => 'b',
+  '-.-.' => 'c',
+  '-..' => 'd',
+  '.' => 'e',
+  '..-.' => 'f',
+  '--.' => 'g',
+  '....' => 'h',
+  '..' => 'i',
+  '.---' => 'j',
+  '-.-' => 'k',
+  '.-..' => 'l',
+  '--' => 'm',
+  '-.' => 'n',
+  '---' => 'o',
+  '.--.' => 'p',
+  '--.-' => 'q',
+  '.-.' => 'r',
+  '...' => 's',
+  '-' => 't',
+  '..-' => 'u',
+  '...-' => 'v',
+  '.--' => 'w',
+  '-..-' => 'x',
+  '-.--' => 'y',
+  '--..' => 'z',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '9',
+  '-----' => '0'
+}.freeze
 
 # convert characters to uppercase
 def decode_char(morse_char)
-  return morse_to_alpha[morse_char].upcase
+  MORSE_CODE[morse_char].upcase
 end
 
 # Define a method that takes a Morse code word as a string and returns its decoded string representation
-def decode_word(morse_word) 
-  morse_chars = morse_word.split(" ")  
-  decoded_chars = morse_chars.map { |morse_char| morse_to_alpha[morse_char] }
-  return decoded_chars.join("")
+def decode_word(morse_word)
+  morse_chars = morse_word.split
+  decoded_chars = morse_chars.map { |morse_char| MORSE_CODE[morse_char] }
+  decoded_chars.join
+end
+
+# Define a method that takes a Morse code message as a string and returns its decoded string representation
+def decode(morse_message)
+  morse_words = morse_message.split('   ')
+
+  decoded_words = morse_words.map do |morse_word|
+    morse_chars = morse_word.split
+
+    decoded_chars = morse_chars.map { |morse_char| MORSE_CODE[morse_char] }
+
+    decoded_chars.join
+  end
+
+  decoded_words.join(' ')
 end
