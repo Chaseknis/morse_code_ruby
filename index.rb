c77683d3ae0886626c8812b5121e3503a2e23668
chaseknis <chase6th@gmail.com>
2023-04-05 16:36:06 +0200

    Update index.rb

--- index.rb
+++ index.rb
@@ -45,7 +45,7 @@ end
 # Define a method that takes a Morse code word as a string and returns its decoded string representation
 def decode_word(morse_word)
   morse_chars = morse_word.split
-  decoded_chars = morse_chars.map { |morse_char| MORSE_CODE[morse_char] }
+  decoded_chars = morse_chars.map { |morse_char| decode_char(morse_char) }
   decoded_chars.join
 end
 
@@ -54,12 +54,10 @@ def decode(morse_message)
   morse_words = morse_message.split('   ')
 
   decoded_words = morse_words.map do |morse_word|
-    morse_chars = morse_word.split
-
-    decoded_chars = morse_chars.map { |morse_char| MORSE_CODE[morse_char] }
-
-    decoded_chars.join
+    decode_word(morse_word)
   end
 
   decoded_words.join(' ')
 end
+
+print decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
